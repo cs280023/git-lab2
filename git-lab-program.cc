680630cf47f8c0138dcb1d89233fefb19b666c69
Caleb Strong <cs280023@ohio.edu>
2026-01-20 12:06:39 -0500

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Caleb Strong
+ *        Date: 1/20/2026
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/ cout << "Hello Git!!" << endl;
+    return 0;
+}// main
